48e6f7f2d6b9fdf8cbe050d3bbde68b961f73a58
elwillan <williamgarnica@ingenieros.com>
2021-06-28 21:12:15 -0500

    se cargan imagenes faltantes

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 function MiNavegacion() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="IndexUI" component={IndexUI} options={{ title: 'Prueba Ingeneo William Garnica', }} />
+      <Stack.Screen name="IndexUI" component={IndexUI} options={{ title: 'Prueb55555a Ingeneo William Garnica', }} />
       <Stack.Screen name="ConsideracionesDelSistemaUI" component={ConsideracionesDelSistemaUI} options={{ title: 'Prueba Ingeneo William Garnica.' }} />
       <Stack.Screen name="GestionarSalasUI" component={GestionarSalasUI} options={{ title: 'Prueba Ingeneo William Garnica.' }} />
       <Stack.Screen name="CrearSalaUI" component={CrearSalaUI} options={{ title: 'Prueba Ingeneo William Garnica.' }} />
